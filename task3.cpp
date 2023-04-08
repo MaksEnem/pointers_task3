f5638aa10ecd42a7effac039d0e6616e0feb012e
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-04-08 20:07:15 +1100

    'second'

--- task3.cpp
+++ task3.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 
-bool is_match(std::string text, std::string word, int startPos) {
-    for (int i = 0; i < word.length(); ++i) {
-        if (i + startPos >= text.length() || word[i] != text[i + startPos]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int find(std::string text, std::string word) {
-    for (int startPos = 0; startPos < text.length(); ++startPos) {
-        if (is_match(text, word, startPos)) {
-            return startPos;
-        }
-    }
-    return -1;
-}
-
-void found() { std::cout << "Found!" << std::endl; }
-
 int main() {
-    std::string text;
-    std::string word;
 
-    std::cout << "Enter the text: ";
-    std::cin >> text;
-    std::cout << "Enter the word: ";
-    std::cin >> word;
+    char text[] = "Hello world";
+    char word[] = "wor";   
+    
+    bool found = false;
 
-    if (find(text, word) >= 0) {
-        found();
+    int startPos = 0;
+    while (*(text + startPos) != '\0') {
+
+        bool match = true;
+        int i = 0;
+        while (*(word + i) != '\0') {
+
+            if (i + startPos >= sizeof(text) || word[i] != text[i + startPos]) {
+                match = false;
+                break;
+            }
+            ++i;
+        }
+        if (match) {
+            found = true;
+            break;
+        }
+        ++startPos;
+    }
+    if (found) {
+        std::cout << "Yes\n";
+    }
+    else {
+        std::cout << "No\n";
     }
 }
